d3d3b8a7dc4a39e048e074eb7adfcc5c078784c6
Arshad Javeed <37022146+arshad171@users.noreply.github.com>
2023-11-20 19:56:24 +0100

    scripts

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -14,20 +14,20 @@ model_file="$5"
 test_data_file="$6"
 predictions_file="$7"
 
-# # Run data_ingestion.py
-# echo "Starting data ingestion..."
-# mkdir $raw_data_file
-# python src/data_ingestion.py --start_date="$start_date" --end_date="$end_date" --output_path="$raw_data_file"
+# Run data_ingestion.py
+echo "Starting data ingestion..."
+mkdir $raw_data_file
+python src/data_ingestion.py --start_date="$start_date" --end_date="$end_date" --output_path="$raw_data_file"
 
-# # Run data_processing.py
-# echo "Starting data processing..."
-# mkdir $processed_data_file
-# python src/data_processing.py --input_path="$raw_data_file" --output_path="$processed_data_file"
+# Run data_processing.py
+echo "Starting data processing..."
+mkdir $processed_data_file
+python src/data_processing.py --input_path="$raw_data_file" --output_path="$processed_data_file"
 
-# # Run model_training.py
-# echo "Starting model training..."
-# mkdir $model_file
-# python src/model_training.py --input_file="$processed_data_file" --model_file="$model_file"
+# Run model_training.py
+echo "Starting model training..."
+mkdir $model_file
+python src/model_training.py --input_file="$processed_data_file" --model_file="$model_file"
 
 # Run model_prediction.py
 echo "Starting prediction..."
